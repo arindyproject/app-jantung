a31971d79d260986c6db54579cb7f718011c4232
arindyproject <ajiariadam@gmail.com>
2024-02-06 20:28:30 +0700

    sdfsdf

---
+++ pages/About.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("#Welcome to my Project! 👋")
+
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        hallo
+        
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
