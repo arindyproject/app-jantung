a1fce831b57d3e136e6d1609dca92255840ae779
arindyproject <ajiariadam@gmail.com>
2024-02-07 13:25:29 +0700

    fsdfsdaf

--- Hello.py
+++ Hello.py
@@ -14,11 +14,29 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
 
 LOGGER = get_logger(__name__)
 
 
+
 def run():
+    #data==================================================================
+    data = {
+        'name' : ['nama'],
+        'age': [0],
+        'sex': [0],
+        'cp': [0],
+        'trestbps': [0],
+        'chol': [0],
+        'fbs': [0],
+        'restecg': [0],
+        'thalach': [0],
+        'exang': [0],
+        'oldpeak': [0]
+    }
+    #data==================================================================
+
     #intro==================================================================
     st.set_page_config(
         page_title="Hello",
@@ -36,61 +54,97 @@ def run():
     #intro==================================================================
 
     #input==================================================================
+
     name = st.text_input('Siapa Nama Anda?')
+    data['name'] = [name]
 
     #age--------------------------------------------------------------------
     age = st.number_input('Berapakah Usia Anda? (dalam tahun)', min_value=0)
+    data['age'] = [age]
     #age--------------------------------------------------------------------
 
     #sex--------------------------------------------------------------------
+    sex_options = {"Laki-Laki": 1, "Perempuan": 0}
     sex = st.radio(
         "Jenis Kelamin",
-        key="visibility",
-        options=["Laki-Laki", "Perempuan"],
+        key="sex",
+        options=list(sex_options.keys()),
     )
+    sex_val     = sex_options[sex]
+    data['sex'] = [sex_val]
+    st.write("You selected: ", sex, ' : ', sex_val)
     #sex--------------------------------------------------------------------
 
     #cp---------------------------------------------------------------------
     #Chest Pain = nyeri dada
-    cp = st.slider('Berapakah skala nyeri dada anda?', 0, 3)
+    cp_options = {
+        "Nyeri dada kiri seperti tertindih atau nyeri dada kiri berat": 0, 
+        "Nyeri dada kiri tidak khas": 1,
+        "Nyeri dada selain di dada kiri" : 2,
+        "Tidak ada keluhan" : 3
+    }
+    cp = st.radio(
+        "Berapakah skala nyeri dada anda?",
+        key="cp",
+        options=list(cp_options.keys()),
+    )
+    cp_val      = cp_options[cp]
+    data['cp']  = [cp_val]
+    st.write("You selected: ", cp, ' : ', cp_val)
     #cp---------------------------------------------------------------------
 
     #trestbps---------------------------------------------------------------
     #Resting blood pressure = tekanan darah saat istirahat
     trestbps = st.number_input('Berapakah tekanan darah anda saat istirahat?', min_value=0)
+    trestbps_val = 0
+    if(trestbps >= 120):
+        trestbps_val = 1
+    data['trestbps']  = [trestbps_val]
+    st.write("You selected: ", trestbps, ' : ', trestbps_val)
     #trestbps---------------------------------------------------------------
 
     #chol--------------------------------------------------------------------
     #Chol = serum kolesterol
     chol = st.number_input('Berapakah serum kolesterol anda?', min_value=0)
+    data['chol']  = [chol]
     #chol--------------------------------------------------------------------
 
     #fbs---------------------------------------------------------------------
     #Fbs = fasting blood sugar atau gula darah puasa
     fbs = st.number_input('Berapakah fasting blood sugar atau gula darah puasa anda?', min_value=0)
+    data['fbs']  = [fbs]
     #fbs---------------------------------------------------------------------
-
-    #restecg-----------------------------------------------------------------
-    #Restecg = rekam jantung (ekg) 
-    restecg = st.number_input('rekam jantung (ekg)  anda?', min_value=0)
-    #restecg-----------------------------------------------------------------
 
     #thalach-----------------------------------------------------------------
     #Thalach = maksimum denyut jantung
     thalach = st.number_input('Berapakah maksimum denyut jantung anda?', min_value=0)
+    data['thalach']  = [thalach]
     #thalach-----------------------------------------------------------------
 
     #exang-------------------------------------------------------------------
     #Exang = nyeri dada karena aktivitas fisik
-    exang = st.number_input('Berapakah nyeri dada karena aktivitas fisik anda?', min_value=0)
+    exang_options = {"Ya": 1, "Tidak": 0}
+    exang = st.radio(
+        "Apakah aktivitas fisik menyebabkan nyeri dada kiri?",
+        key="exang",
+        options=list(exang_options.keys()),
+    )
+    exang_val = exang_options[exang]
+    data['exang']  = [exang_val]
+    st.write("You selected: ", exang, ' : ', exang_val)
     #exang-------------------------------------------------------------------
 
     #oldpeak-----------------------------------------------------------------
     #Oldpeak = st depresi karena aktivitas fisik
     oldpeak = st.number_input('Berapakah depresi karena aktivitas fisik anda?', min_value=0)
+    data['oldpeak']  = [oldpeak]
     #oldpeak-----------------------------------------------------------------
 
     #input==================================================================
+
+    st.write("##### Ringkasan Data")
+    df = pd.DataFrame(data)
+    st.write(df)
 
 if __name__ == "__main__":
     run()
