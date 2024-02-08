5dee017a7d339d676c68826488be6295e4afb8fc
arindyproject <ajiariadam@gmail.com>
2024-02-08 08:59:20 +0700

    dgds

--- Hello.py
+++ Hello.py
@@ -182,6 +182,7 @@ def run():
             my_bar = st.progress(0, text=progress_text)
 
             for percent_complete in range(100):
+                time.sleep(0.001)
                 my_bar.progress(percent_complete + 1, text=progress_text)
             time.sleep(1)
             my_bar.empty()
